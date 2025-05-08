f3758ae4a85e69fad3a104ffcdc1b8f245fff3b3
Ductrung03 <Ductrung021103@gmail.com>
2025-05-08 16:56:38 +0700

    Add project files.

---
+++ QuanLyGiaoVienCSDLNC/Controllers/SamplePageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyGiaoVienCSDLNC.Controllers
+{
+    public class SamplePageController : Controller
+    {
+        public IActionResult Page1()
+        {
+            return View();
+        }
+        public IActionResult Page2()
+        {
+            return View();
+        }
+    }
+}
